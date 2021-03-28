47f92db1b6d91eab2e43cca631794c38f6ae4a9b
Thomas Gazagnaire <thomas@gazagnaire.org>
2021-03-28 22:01:03 +0200

    WIP

---
+++ src/irmin/id_intf.ml
@@ -0,0 +1,33 @@
+(*
+ * Copyright (c) 2013-2021 Thomas Gazagnaire <thomas@gazagnaire.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *)
+
+module type S = sig
+  include Type.S
+
+  type hash
+
+  val short_hash : t -> int
+  val to_hash : t -> hash
+  val of_hash : hash -> t
+
+  module Hash : Hash.S with type t = hash
+end
+
+module type Id = sig
+  module type S = S
+
+  module Identity (H : Hash.S) : S with type t = H.t and type hash = H.t
+end
